bebf49bf31b59e4f107480e4dbe99413d026d8ce
mauricio <mauricioestupian@gmail.com>
2024-11-15 20:00:32 -0500

    cambios 15 nov modelos y componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PadreComponent } from './padre/padre.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,PadreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angularadsonoche';
+  title = 'angularadsonoche Gol Colombia';
+  subtitulo = 'Quintero'
+
 }
