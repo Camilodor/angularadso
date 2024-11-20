fa3f68dfd38666d6ef0ba6135ca2ad3233729629
mauricio <mauricioestupian@gmail.com>
2024-11-19 19:47:10 -0500

    clase 19 nov

--- src/app/cuerpo/cuerpo.component.ts
+++ src/app/cuerpo/cuerpo.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { PadreComponent } from '../padre/padre.component';
 
 @Component({
   selector: 'app-cuerpo',
   standalone: true,
-  imports: [],
+  imports: [PadreComponent],
   templateUrl: './cuerpo.component.html',
   styleUrl: './cuerpo.component.css'
 })
 export class CuerpoComponent {
+  valor = 0;
+
+  incrementar() {
+    this.valor++;
+  }
+
+  disminuir() {
+    this.valor--;
+  }
 
 }
