3ca89be7e82ff268bc6b103a8501b45ab9266a46
Mauricio E <mauricio_estupian@yahoo.com>
2024-12-05 20:58:48 -0500

    clase 2 05 dic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,18 @@ import { Routes } from '@angular/router';
 import { CuerpoComponent } from './cuerpo/cuerpo.component';
 import { PadreComponent } from './usuarios/padre/padre.component';
 import { PaginaComponent } from './pagina/pagina.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-  { path: '', component: CuerpoComponent},
-  { path: 'padre',component: PadreComponent}
+  {path: '', redirectTo: 'adso', pathMatch: 'full'},
+  {path:'login',component:LoginComponent},
+  {path: 'adso', component: CuerpoComponent,
+    children:[
+      {path: '', component: PaginaComponent},
+      { path: 'padre',component: PadreComponent},
+      { path: 'pagina',component: PaginaComponent},
+    ]
+  },
+  
   
 ]
--- src/app/cuerpo/cuerpo.component.ts
+++ src/app/cuerpo/cuerpo.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { PaginaComponent } from "../pagina/pagina.component";
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-cuerpo',
   standalone: true,
-  imports: [PaginaComponent],
+  imports: [RouterOutlet,PaginaComponent, HeaderComponent, FooterComponent],
   templateUrl: './cuerpo.component.html',
   styleUrl: './cuerpo.component.css'
 })
